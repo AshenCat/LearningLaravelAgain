1f6d3b4ce474bb100dae800ceb73e17d5e83354e
Klifford Agujar <forddagujar95@gmail.com>
2019-07-11 18:33:16 -0400

    finally uses saved photo & hides add post and edit profile for non auth

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,13 +4,20 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
-            <img src="https://instagram.fyyz1-1.fna.fbcdn.net/vp/885953fceb7ee32a2623fa424f110183/5DA8C4B7/t51.2885-19/s150x150/26066874_1697507040306984_2164242150827491328_n.jpg?_nc_ht=instagram.fyyz1-1.fna.fbcdn.net" class="rounded-circle">
+            <img src="/storage/{{$user->profile->image}}" class="rounded-circle w-100">
         </div>
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1>{{ $user->username}}</h1>
-                <a href="/posts/create">Add New Post</a>
+                @can('update', $user->profile)
+                    <a href="/posts/create">Add New Post</a>
+                @endcan
             </div>
+
+            @can('update', $user->profile)
+                <a href="/profile/{{$user->id}}/edit">Edit Profile</a>
+            @endcan
+
             <div class="d-flex">
                 <div class="pr-5"><strong>{{$user->posts->count()}}</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> followers</div>
